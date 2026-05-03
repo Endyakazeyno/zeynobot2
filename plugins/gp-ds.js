a3da67eb032bee02c09fd747e994f8c5ac242115
Giuse <giuseakanex@gmail.com>
2026-05-03 14:50:02 +0200

    Update gp-ds.js

--- plugins/gp-ds.js
+++ plugins/gp-ds.js
@@ -3,7 +3,6 @@ import path from 'path'
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, isOwner, isAdmin }) => {
-    // Controllo Permessi
     if (!m.isGroup && !isOwner) return; 
     if (m.isGroup && !isAdmin && !isOwner) return;
 
@@ -12,7 +11,6 @@ let handler = async (m, { conn, isOwner, isAdmin }) => {
         let tempPath = './temp/'
         let deletedFiles = 0
 
-        // Pulizia cartella Session (escluso creds.json)
         if (fs.existsSync(sessionPath)) {
             let files = fs.readdirSync(sessionPath)
             for (let file of files) {
@@ -25,7 +23,6 @@ let handler = async (m, { conn, isOwner, isAdmin }) => {
             }
         }
         
-        // Pulizia cartella Temp
         if (fs.existsSync(tempPath)) {
             let tempFiles = fs.readdirSync(tempPath)
             for (let file of tempFiles) {
@@ -37,11 +34,8 @@ let handler = async (m, { conn, isOwner, isAdmin }) => {
         }
 
         let finalCount = deletedFiles > 0 ? deletedFiles : Math.floor(Math.random() * 200) + 500;
-
-        // Testo sotto al quadrato
         let text = `✅ 𝐄𝐥𝐢𝐦𝐢𝐧𝐚𝐭𝐢 ${finalCount} 𝐟𝐢𝐥𝐞 𝐝𝐢 𝐬𝐞𝐬𝐬𝐢𝐨𝐧𝐞.`.trim()
 
-        // Estrazione immagine per la AdReply
         let profilePicture;
         try { 
             profilePicture = await conn.profilePictureUrl(conn.user.jid, 'image'); 
@@ -57,12 +51,16 @@ let handler = async (m, { conn, isOwner, isAdmin }) => {
         };
         let imageBuffer = await getBuffer(profilePicture);
 
-        // Invio con meccanica AdReply + Inoltro Canale
         await conn.sendMessage(m.chat, { 
             text: text,
             contextInfo: {
-                forwardingScore: 999,
                 isForwarded: true,
+                forwardingScore: 999,
+                forwardedNewsletterMessageInfo: {
+                    newsletterJid: '120363233544482011@newsletter', 
+                    serverMessageId: 100,
+                    newsletterName: "• 𝐍𝐄𝐖 𝐄𝐑𝐀 •"
+                },
                 externalAdReply: {
                     title: '𝐂𝐥𝐞𝐚𝐫𝐞𝐝 𝐬𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥𝐥𝐲 ✅', 
                     body: '𝐍𝐄𝐖 𝐄𝐑𝐀 • 𝐒𝐲𝐬𝐭𝐞𝐦',
