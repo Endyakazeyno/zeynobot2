4520fc4dfbf0075339fce39ae2ade7dd09bade50
Giuse <giuseakanex@gmail.com>
2026-05-04 16:21:33 +0200

    Update creatore-addmod.js

--- plugins/creatore-addmod.js
+++ plugins/creatore-addmod.js
@@ -25,7 +25,7 @@ const handler = async (m, { conn, usedPrefix, command }) => {
   }
 
   const name = '@' + who.split('@')[0];
-  const caption = `${name} 𝐨𝐫𝐚 è 𝐮𝐧 𝐧𝐮𝐨𝐯𝐨 𝐯𝐢𝐩! 𝐁𝐞𝐧𝐯𝐞𝐧𝐮𝐭𝐨 𝐧𝐞𝐥𝐥𝐨 𝐬𝐭𝐚𝐟𝐟, 𝐩𝐞𝐫 𝐯𝐞𝐝𝐞𝐫𝐞 𝐢 𝐭𝐮𝐨𝐢 𝐜𝐨𝐥𝐥𝐞𝐠𝐡𝐢 𝐮𝐬𝐚 .𝐯𝐢𝐩𝐥𝐢𝐬𝐭!`.trim();
+  const caption = `${name} 𝐨𝐫𝐚 è 𝐮𝐧 𝐧𝐮𝐨𝐯𝐨 𝐯𝐢𝐩! 𝐁𝐞𝐧𝐯𝐞𝐧𝐮𝐭𝐨 𝐧𝐞𝐥𝐥𝐨 𝐬𝐭𝐚𝐟𝐟, 𝐬𝐜𝐨𝐩𝐫𝐢 𝐢 𝐜𝐨𝐦𝐚𝐧𝐝𝐢 𝐜𝐨𝐧 ${Usedprefix}𝐦𝐞𝐧𝐮𝐯𝐢𝐩!`.trim();
 
   await conn.sendMessage(m.chat, {
       text: caption,
