d713ec43ccb4c65c02bee9fc29c02a3f7593dfc5
Giuse <giuseakanex@gmail.com>
2026-05-04 09:51:58 +0200

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -3,10 +3,10 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
     // Definizione del testo con font Bold Sans e Italic per un look premium
     let text = `
 *𝐍𝐄𝐖 𝐄𝐑𝐀*
-_The Next Generation Core_
+The Next Generation Core
 
-Ciao @${m.sender.split('@')[0]},
-Seleziona una categoria qui sotto per visualizzare i comandi disponibili.
+𝐂𝐢𝐚𝐨 @${m.sender.split('@')[0]},
+𝐒𝐞𝐥𝐞𝐳𝐢𝐨𝐧𝐚 𝐮𝐧𝐚 𝐜𝐚𝐭𝐞𝐠𝐨𝐫𝐢𝐚 𝐪𝐮𝐢 𝐬𝐨𝐭𝐭𝐨 𝐩𝐞𝐫 𝐯𝐢𝐬𝐮𝐚𝐥𝐢𝐳𝐳𝐚𝐫𝐞 𝐢 𝐜𝐨𝐦𝐚𝐧𝐝𝐢 𝐝𝐢𝐬𝐩𝐨𝐧𝐢𝐛𝐢𝐥𝐢!
 
 *STATS*
 • *Stato:* Operativo
@@ -15,16 +15,16 @@ Seleziona una categoria qui sotto per visualizzare i comandi disponibili.
 
     // Array di bottoni (senza Download e Ricerca)
     const buttons = [
-      { buttonId: `${_p}menustrumenti`, buttonText: { displayText: '🛠️ Strumenti' }, type: 1 },
-      { buttonId: `${_p}menueuro`, buttonText: { displayText: '💰 Economia' }, type: 1 },
-      { buttonId: `${_p}menugiochi`, buttonText: { displayText: '🎮 Giochi' }, type: 1 },
-      { buttonId: `${_p}menugruppo`, buttonText: { displayText: '👥 Gruppo' }, type: 1 },
-      { buttonId: `${_p}menucreatore`, buttonText: { displayText: '👨‍💻 Creatore' }, type: 1 },
-      { buttonId: `${_p}menuvip`, buttonText: { displayText: '🛡️ VIP' }, type: 1 },
-      { buttonId: `${_p}menuspeciale`, buttonText: { displayText: '🪩 Speciale' }, type: 1 },
-      { buttonId: `${_p}menusicurezza`, buttonText: { displayText: '🚨 Sicurezza' }, type: 1 },
-      { buttonId: `${_p}contronuke on`, buttonText: { displayText: '☢️ Anti-Nuke' }, type: 1 },
-      { buttonId: `${_p}feedback`, buttonText: { displayText: '✍️ Feedback' }, type: 1 }
+      { buttonId: `${_p}menustrumenti`, buttonText: { displayText: '🛠️ 𝐒𝐭𝐫𝐮𝐦𝐞𝐧𝐭𝐢' }, type: 1 },
+      { buttonId: `${_p}menueuro`, buttonText: { displayText: '💰 𝐄𝐜𝐨𝐧𝐨𝐦𝐢𝐚' }, type: 1 },
+      { buttonId: `${_p}menugiochi`, buttonText: { displayText: '🎮 𝐆𝐢𝐨𝐜𝐡𝐢' }, type: 1 },
+      { buttonId: `${_p}menugruppo`, buttonText: { displayText: '👥 𝐆𝐫𝐮𝐩𝐩𝐨' }, type: 1 },
+      { buttonId: `${_p}menucreatore`, buttonText: { displayText: '👨‍💻 𝐂𝐫𝐞𝐚𝐭𝐨𝐫𝐞' }, type: 1 },
+      { buttonId: `${_p}menuvip`, buttonText: { displayText: '🛡️ 𝐕𝐈𝐏 ' }, type: 1 },
+      { buttonId: `${_p}menuspeciale`, buttonText: { displayText: '🪩 𝐒𝐩𝐞𝐜𝐢𝐚𝐥𝐞' }, type: 1 },
+      { buttonId: `${_p}menusicurezza`, buttonText: { displayText: '🚨 𝐒𝐢𝐜𝐮𝐫𝐞𝐳𝐳𝐚 ' }, type: 1 },
+      { buttonId: `${_p}contronuke on`, buttonText: { displayText: '☢️ 𝐀𝐧𝐭𝐢-𝐍𝐮𝐤𝐞' }, type: 1 },
+      { buttonId: `${_p}feedback`, buttonText: { displayText: '✍️ 𝐅𝐞𝐞𝐝𝐛𝐚𝐜𝐤 ' }, type: 1 }
     ];
 
     // Invio del messaggio
